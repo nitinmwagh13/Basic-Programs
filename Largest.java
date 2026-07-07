75b2e760ec1374ca296bcd1dbdefc69fc309e277
Nitin M. Wagh <nitinmwagh13@gmail.com>
2026-07-07 18:17:16 +0530

    update nitin
    logic change

--- Largest.java
+++ Largest.java
@@ -1,5 +1,12 @@
 public class Largest {
     public static void main(String[] args) {
-        
+    int a=457;
+    int b=56;
+    if (a>b){
+        System.out.println(" a is lagger than b");
+    }
+    else {
+        System.out.println("b is lager than a");
+    }
     }
 }
