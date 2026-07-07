2302e770653d253ee8383f4dc88e0cd134b71541
Nitin M. Wagh <nitinmwagh13@gmail.com>
2026-07-07 17:03:44 +0530

    Add files via upload

---
+++ swaptwoNum.java
@@ -0,0 +1,16 @@
+public class swaptwoNum {
+    public static void main(String args[]){
+        int a=23;
+        int b=45;
+       int temp=0;
+
+       temp=a;
+       a=b;
+       b=temp;
+       System.out.println(" a is"+a);
+       System.out.println("b is"+b);
+
+
+
+    }
+}
