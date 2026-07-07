975900d7fd493b8df4fb9d9125082d71f29ce87c
Nitin M. Wagh <nitinmwagh13@gmail.com>
2026-07-07 17:04:27 +0530

    Update swaptwoNum.java

--- swaptwoNum.java
+++ swaptwoNum.java
@@ -2,9 +2,9 @@ public class swaptwoNum {
     public static void main(String args[]){
         int a=23;
         int b=45;
-       int temp=0;
+       
 
-       temp=a;
+      int temp=a;
        a=b;
        b=temp;
        System.out.println(" a is"+a);
