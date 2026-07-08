77ff425a8e3e6e695f8bc042b4d7435ebba9de5d
Nitin M. Wagh <nitinmwagh13@gmail.com>
2026-07-08 15:53:14 +0530

    Add files via upload

---
+++ ArmstrongNum.java
@@ -0,0 +1,19 @@
+public class ArmstrongNum {
+    public static void main(String[] args) {
+        int num=153;
+        int temp=num;
+        int sum=0;
+        while(num!=0){
+            int digit=num%10;
+            sum=sum+digit*digit*digit;
+            num=num/10;
+        }
+        if(sum==temp)
+            System.out.println("Armstrong number");
+        else
+        System.out.println("not armstrong number");
+    
+        }
+
+    }
+    
