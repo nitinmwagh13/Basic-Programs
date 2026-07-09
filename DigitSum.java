fd4d7428d4b426b0c3f036c55264bed8f49d8c4f
Nitin M. Wagh <nitinmwagh13@gmail.com>
2026-07-09 14:05:52 +0530

    Add files via upload

---
+++ DigitSum.java
@@ -0,0 +1,14 @@
+public class DigitSum {
+    public static void main(String[] args) {
+        int num=12345;
+        int sum=0;
+        while(num>0){
+            int rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+            
+        }
+        System.out.println(sum);
+    }
+    
+}
